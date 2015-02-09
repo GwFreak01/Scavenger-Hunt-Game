7ccdb62f95e89257b75b3ac8d8b81d4c6c0eecd7
Thanh Pham <GWFreak01@Thanhs-MacBook-Pro.local>
2015-02-09 14:08:21 -0500

    First Rev

---
+++ Scavenger Hunt Game/ScavengerHuntItem.swift
@@ -0,0 +1,9 @@
+//
+//  Scavenger Hunt Item.swift
+//  Scavenger Hunt Game
+//
+//  Created by Thanh Pham on 2/2/15.
+//  Copyright (c) 2015 Thanh Pham. All rights reserved.
+//
+
+import Foundation
